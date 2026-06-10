2ccc814a2d267011307a1ee5cd0d2e67a03013da
Erickson Faña Morales <erickfm14955@hotmail.com>
2026-06-10 10:39:04 -0400

    Prueba de descarga de xlsx

--- Pagination Project/wwwroot/js/assignmentImportModal.js
+++ Pagination Project/wwwroot/js/assignmentImportModal.js
@@ -110,3 +110,33 @@ function escapeHtml(value) {
         .replaceAll('"', "&quot;")
         .replaceAll("'", "&#039;");
 }
+
+window.downloadFileFromBase64 = (fileName, contentType, base64Data) => {
+    const byteCharacters = atob(base64Data);
+    const byteNumbers = new Array(byteCharacters.length);
+
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+
+    const blob = new Blob([byteArray], {
+        type: contentType || "application/octet-stream"
+    });
+
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = fileName;
+    link.style.display = "none";
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    setTimeout(() => {
+        URL.revokeObjectURL(url);
+    }, 1000);
+};
