55825126f4d462ae20730e544f1b2b244397b253
Erickson Faña Morales <erickfm14955@hotmail.com>
2026-06-21 00:01:50 -0400

    Prueba del modal del dashboard

--- Pagination Project/wwwroot/js/site.js
+++ Pagination Project/wwwroot/js/site.js
@@ -3,3 +3,15 @@
         return window.matchMedia("(max-width: 992px)").matches;
     }
 };
+
+window.thryvModalScroll = {
+    open: function () {
+        document.documentElement.classList.add("thryv-modal-open");
+        document.body.classList.add("thryv-modal-open");
+    },
+
+    close: function () {
+        document.documentElement.classList.remove("thryv-modal-open");
+        document.body.classList.remove("thryv-modal-open");
+    }
+};
